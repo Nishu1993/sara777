b5287e2b5a8b6dfd980f4b000d3114a7492175b6
Nishu1993 <niteshkhandelwal008@gmail.com>
2024-07-08 13:41:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('call-now').addEventListener('click', function() {
+    alert('Call now button clicked!');
+});
+
+document.getElementById('whatsapp').addEventListener('click', function() {
+    alert('Whatsapp button clicked!');
+});
